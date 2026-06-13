7968de14e7792ee7ce662cc67756ec92926f8264
Gaurav-mgr <gauravpulami5@gmail.com>
2026-06-13 18:08:15 +0545

    updated FavIcon of the app

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import "./Header.css"
+import TenziesIcon from "../../assets/TenziesIcon.png"
 
 export default function Header({message, gameWon, gameLose}){
   const style = {
@@ -7,7 +8,11 @@ export default function Header({message, gameWon, gameLose}){
   return(
     <>
       <div className="title">
-        {message.length === 0 && <img src="public/TenziesIcon.png" alt="Tenzies Icon" />}
+        {message.length === 0 && 
+        <img 
+          src={TenziesIcon} 
+          alt="Tenzies Icon" 
+        />}
         <h1 className="title" style={style}>{ message.length > 0 ? message : "Tenzies"}</h1>
       </div>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
